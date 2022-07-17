0a7ccb533ffc609c3e064b6659405822f36b7032
nathanromanelli <nroma009@ucr.edu>
2022-07-17 16:49:42 -0700

    Initial commit

---
+++ template/template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using vi = vector<int>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vll = vector<long long>;
+ 
+#define FOR(i,a,b) = for(int i = a; i < b; ++i)
+#define ROF(i,a,b) = for(int i = a, i >= b; --i)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)x.size()
+#define ll long long
+#define pb push_back
+ 
+void solve(){
+	
+}
+ 
+const int mod = (1e9 + 7);
+ 
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	FOR(t){
+		//cout << "Case #" << i + 1 << ": ";
+		solve();
+	}
+	return 0;
+}
+
+//Check for initialized variables, overflow, and edge cases
